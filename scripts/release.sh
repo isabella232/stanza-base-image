45662df67ecfa3fa141ef7a48ec47c1fde3a9772
jsirianni <joe.sirianni@observiq.com>
2021-07-08 18:54:57 -0400

    tag

--- scripts/release.sh
+++ scripts/release.sh
@@ -4,5 +4,6 @@ set -e
 
 IMAGE="${IMAGE_NAME}:${IMAGE_TAG}"
 echo "${GCLOUD_SERVICE_KEY}" | docker login -u _json_key --password-stdin https://gcr.io/
+docker tag "${IMAGE_NAME}:latest" "${IMAGE}"
 docker tag "${IMAGE}" "gcr.io/observiq-container-images/${IMAGE}"
 docker push "${IMAGE_NAME}:${IMAGE_TAG}"
