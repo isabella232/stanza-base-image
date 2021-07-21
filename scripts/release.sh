74c793091cb32ddc2af637f5c0c187098c4cfcb4
jsirianni <joe.sirianni@observiq.com>
2021-07-21 10:24:24 -0400

    skip release when tag not set

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ -z "$IMAGE_TAG" ]
+then
+      echo "${IMAGE_TAG} not set, skipping release"
+fi
+
 IMAGE="${IMAGE_NAME}:${IMAGE_TAG}"
 echo "${GCLOUD_SERVICE_KEY}" | docker login -u _json_key --password-stdin https://gcr.io/
 docker tag "${IMAGE_NAME}:latest" "${IMAGE}"
