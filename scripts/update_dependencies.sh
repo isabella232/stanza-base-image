929491864046808fdfd2504843bf64f6e1e1cb7c
jsirianni <joe.sirianni@observiq.com>
2021-08-19 14:41:33 -0400

    dockerize the update script in order to get ubuntu 20.10 package versions while running inside ubuntu 20.04 (github actions does not support ubuntu 20.10)

--- scripts/update_dependencies.sh
+++ scripts/update_dependencies.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+apt-get update -qq -o=Dpkg::Use-Pty=0
+apt-get install -qq -o=Dpkg::Use-Pty=0 -y python jq
+
 JSON=$( cat dependencies.json )
+echo "detecting versions for ${JSON}"
 
 for PACKAGE in $( echo "${JSON}" | jq -r 'keys | .[]' ); do
 	VERSION=$( apt-cache policy "${PACKAGE}" | grep -oP '(?<=Candidate:\s)(.+)' )
