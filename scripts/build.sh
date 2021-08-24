9b2382976b2eded2adc82cfef6728dd604e64850
jsirianni <joe.sirianni@observiq.com>
2021-08-24 16:15:16 -0400

    set tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,9 +4,11 @@ set -e
 
 echo "${GCLOUD_SERVICE_KEY}" | docker login -u _json_key --password-stdin https://gcr.io/
 
+TAG="$(git rev-parse --short HEAD)"
+
 docker buildx build \
     --platform linux/amd64,linux/arm64 \
-    --tag "gcr.io/observiq-container-images/stanza-base:$(git rev-parse --short HEAD)" \
+    --tag "gcr.io/observiq-container-images/stanza-base:${TAG}" \
     --build-arg dependencies="$(jq -r 'to_entries | .[] | .key + "=" + .value' dependencies.json)" \
     --push \
     .
