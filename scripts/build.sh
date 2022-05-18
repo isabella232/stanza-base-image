8f49563b4c70f9a000df9d6bbc73de46b759b13f
jsirianni <joe.sirianni@bluemedora.com>
2022-05-18 11:35:21 -0400

    revert test comment

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-#echo "${GCLOUD_SERVICE_KEY}" | docker login -u _json_key --password-stdin https://gcr.io/
+echo "${GCLOUD_SERVICE_KEY}" | docker login -u _json_key --password-stdin https://gcr.io/
 
 TAG="$(git rev-parse --short HEAD)"
 
@@ -10,4 +10,5 @@ docker buildx build \
     --platform linux/amd64,linux/arm64 \
     --tag "gcr.io/observiq-container-images/stanza-base:${TAG}" \
     --build-arg dependencies="$(jq -r 'to_entries | .[] | .key + "=" + .value' dependencies.json | xargs)" \
+    --push \
     .
